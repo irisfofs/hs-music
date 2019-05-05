30d8914d354cfdecaec3e0fd0d7667dd53e82f0a
xiagu <me@andrew.rs>
2019-05-04 23:57:13 -0700

    Change to light theme and use Homestuck colors

--- src/app/timeline.component.ts
+++ src/app/timeline.component.ts
@@ -56,6 +56,12 @@ interface ImportantEvent {
   length?: number;
 }
 
+const HS_RED = '#e00707';
+const HS_BLUE = '#0715cd';
+const HS_LIME = '#4ac925';
+const HS_GREEN = '#168500';
+const HS_GRAY = '#C6C6C6';
+
 function drawActs(chart) {
   /* Act information */
   const everything: {part1: ImportantEvent[], part2: ImportantEvent[]} = {
@@ -64,25 +70,31 @@ function drawActs(chart) {
         title: 'Act 1',
         page: 1,
         subtitle: 'The Note Desolation Plays',
-        color: 'red'
+        color: HS_RED
       },
       {
         title: 'Act 2',
         page: 248,
         subtitle: 'Raise of the Conductor\'s Baton',
-        color: 'red'
+        color: HS_RED
       },
       {
         title: 'Act 3',
         page: 759,
         subtitle: 'Insane Corkscrew Haymakers',
-        color: 'red'
+        color: HS_RED
+      },
+      {
+        title: 'Intermission',
+        page: 1154,
+        subtitle: 'Don\'t Bleed on the Suits',
+        color: HS_GREEN,
       },
       {
         title: 'Act 4',
         page: 1358,
         subtitle: 'Flight of the Paradox Clones',
-        color: 'red'
+        color: HS_RED
       },
     ],
     part2: [
@@ -96,93 +108,93 @@ function drawActs(chart) {
         title: 'Act 5 Act 2',
         page: 2626,
         subtitle: 'He is already here.',
-        color: 'red'
+        color: HS_RED
       },
       {title: 'EOA5', page: 4109, subtitle: 'Cascade.', color: 'black'},
       {
         title: 'Act 6 Act 1',
         page: 4113,
         subtitle: 'Through Broken Glass',
-        color: 'green'
+        color: HS_GRAY
       },
       {
         title: 'Act 6 Intermission 1',
         page: 4295,
         subtitle: 'corpse party',
-        color: 'gray'
+        color: HS_LIME
       },
       {
         title: 'Act 6 Act 2',
         page: 4419,
         subtitle: 'Your shit is wrecked.',
-        color: 'green'
+        color: HS_GRAY
       },
       {
         title: 'Act 6 Intermission 2',
         page: 4667,
         subtitle: 'penis ouija',
-        color: 'gray'
+        color: HS_LIME
       },
-      {title: 'Act 6 Act 3', page: 4820, subtitle: 'Nobles', color: 'green'},
+      {title: 'Act 6 Act 3', page: 4820, subtitle: 'Nobles', color: HS_GRAY},
       {
         title: 'Act 6 Intermission 3',
         page: 5263,
         subtitle: 'Ballet of the Dancestors',
-        color: 'gray'
+        color: HS_LIME
       },
-      {title: 'Act 6 Act 4', page: 5438, subtitle: 'Void', color: 'green'},
+      {title: 'Act 6 Act 4', page: 5438, subtitle: 'Void', color: HS_GRAY},
       {
         title: 'Act 6 Intermission 4',
         page: 5441,
         subtitle: 'Dead',
-        color: 'gray'
+        color: HS_LIME
       },
       {
         title: 'Act 6 Act 5',
         page: 5571,
         subtitle: 'Of Gods and Tricksters',
-        color: 'green'
+        color: HS_GRAY
       },
       {
         title: 'Act 6 Intermission 5',
         page: 5927,
         subtitle: 'I\'M PUTTING YOU ON SPEAKER CRAB.',
-        color: 'gray'
+        color: HS_LIME
       },
-      {title: 'Act 6 Act 6', page: 6243, subtitle: '', color: 'green'},
+      {title: 'Act 6 Act 6', page: 6243, subtitle: '', color: HS_GREEN},
       {
         title: 'Act 6 Act 6 Intermission 1',
         page: 6278,
         subtitle: '',
-        color: 'green'
+        color: HS_GRAY
       },
-      {title: 'Act 6 Act 6 Act 2', page: 6475, subtitle: '', color: 'green'},
+      {title: 'Act 6 Act 6 Act 2', page: 6475, subtitle: '', color: HS_GREEN},
       {
         title: 'Act 6 Act 6 Intermission 2',
         page: 6531,
         subtitle: '',
-        color: 'green'
+        color: HS_GRAY
       },
-      {title: 'Act 6 Act 6 Act 3', page: 6853, subtitle: '', color: 'green'},
+      {title: 'Act 6 Act 6 Act 3', page: 6853, subtitle: '', color: HS_GREEN},
       {
         title: 'Act 6 Act 6 Intermission 3',
         page: 6901,
         subtitle: '',
-        color: 'green'
+        color: HS_GRAY
       },
-      {title: 'Act 6 Act 6 Act 4', page: 6921, subtitle: '', color: 'green'},
+      {title: 'Act 6 Act 6 Act 4', page: 6921, subtitle: '', color: HS_GREEN},
       {
         title: 'Act 6 Act 6 Intermission 4',
         page: 6944,
         subtitle: '',
-        color: 'green'
+        color: HS_GRAY
       },
-      {title: 'Act 6 Act 6 Act 5', page: 7409, subtitle: '', color: 'green'},
+      {title: 'Act 6 Act 6 Act 5', page: 7409, subtitle: '', color: HS_GREEN},
       {
         title: 'Act 6 Act 6 Intermission 5',
         page: 7449,
         subtitle: '',
-        color: 'green'
+        color: HS_GRAY
       },
       {title: 'EOA6', page: 8087, subtitle: 'Collide.', color: 'green'},
       {title: 'Act 7', page: 8127, subtitle: '', color: 'white'},
@@ -310,7 +322,7 @@ function d3stuff() {
       .attr('x2', (d) => d.x)
       .attr('y1', () => height / 2)
       .attr('y2', () => height / 2)
-      .attr('stroke', 'white')
+      .attr('stroke', 'black')
       .attr('stroke-width', 1)
       .transition()
       .duration(500)
