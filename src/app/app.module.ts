bd3ae44dcf402b6b230d92f1b872dac5d2885c80
xiagu <me@andrew.rs>
2019-05-04 18:22:55 -0700

    "Functioning" timeline loading with TS + Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {TimelineComponent} from './timeline.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations : [
+    AppComponent,
+    TimelineComponent,
   ],
-  imports: [
-    BrowserModule
+  imports : [
+    BrowserModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers : [],
+  bootstrap : [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
